0af127dd91d9a1f3c5c4ed97ad7c40b9f0e1b0d0
Diego Carrillo <diegoacc051@gmail.com>
2025-03-19 13:05:19 -0600

    Added generate matrix & clean matrix functions

--- braille_to_text.py
+++ braille_to_text.py
@@ -24,3 +24,46 @@ def get_string(path: str) -> str:
     # Extract text from processed image
     text = pytesseract.image_to_string(laplacian_abs, config='--psm 6')
     return text
+
+def generate_letters(braille: str) -> list[list[str]]:
+    length_braille = 0
+    for i in braille:
+        if i != " ":
+            length_braille += 1
+        if i == "\n":
+            break
+    
+    letter_count = length_braille // 2
+    
+    word_matrix = [[] for _ in range(letter_count)] 
+ 
+    plain_text = ""
+    for i in braille:
+        if i != " " and i != "\n":
+            plain_text += i
+    
+    index_matrix = 0
+    for i in range(0, letter_count * 2, 2):
+        word_matrix[index_matrix].append(plain_text[i])
+        word_matrix[index_matrix].append(plain_text[i + 1])
+        word_matrix[index_matrix].append(plain_text[i + length_braille - 1])
+        word_matrix[index_matrix].append(plain_text[i + length_braille])
+        word_matrix[index_matrix].append(plain_text[i + (length_braille * 2) - 2])
+        word_matrix[index_matrix].append(plain_text[i + (length_braille * 2) - 1])
+        index_matrix += 1
+    
+    word_matrix = clean_matrix(word_matrix)
+
+    for row in word_matrix:
+        print(row)
+
+    return word_matrix
+
+def clean_matrix(word_matrix: list[list[str]]) -> list[list[str]]:
+    for i, value in enumerate(word_matrix):
+        for j, char in enumerate(value):
+            if char == "O":
+                continue
+            else:
+                word_matrix[i][j] = "X"
+    return word_matrix
